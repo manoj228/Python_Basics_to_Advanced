ddee30f3153b2294257d2d2bf1dd76c999a78c60
manoj <manojanbu2017@gmail.com>
2025-10-22 11:20:01 +0530

    math library import

---
+++ mathlib.py
@@ -0,0 +1,32 @@
+'''
+import math
+
+-> sqrt(number)
+-> floor(number)
+-> ceil(number)
+-> pow(num, power_val)
+> math.pi
+-> math.e 
+
+
+----------------------------------------------
+
+include only specific library
+
+from math import sqrt, pow
+'''
+
+import math as m
+
+x = m.sqrt(25)
+
+print('Sqrt of 25 =', x)
+print('Ceil of 2.5 =', m.ceil(25.2))
+print('Power of 3^2 ', m.pow(3, 2))
+print('PI =', m.pi)
+
+from math import sqrt 
+
+print('Sqrt of 49 =', sqrt(49))
+
+
