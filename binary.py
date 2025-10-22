204768dd591b7aa51fb513c91a236fe93f1636ad
manoj <manojanbu2017@gmail.com>
2025-10-22 11:08:30 +0530

    decimal to bin/oct/hex

---
+++ binary.py
@@ -0,0 +1,15 @@
+'''
+bin(number) -> converts decimal to binary  
+
+eg: bin(10) # output as 0b1010 
+
+oct(number) -> converts decimal to octal
+
+hex(number) -> converts decimal to hex
+'''
+
+print(bin(10))
+
+print(oct(10))
+
+print(hex(10))
